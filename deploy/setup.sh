4c0ed467447a0563c7eca4cb7a0ca9948d762860
elaganesh25 <elaganesh25@gmail.com>
2026-03-16 22:24:10 -0500

    Update file copy path in setup script

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -51,7 +51,7 @@ sudo mkdir -p /var/www/jerney
 sudo chown -R $USER:$USER /var/www/jerney
 
 # Copy project files (assumes you've transferred them to /home/ubuntu/AWS-DevSecOps)
-cp -r /home/ubuntu/AWS-DevSecOps/* /var/www/jerney/
+cp -r ~/Jerney/* /var/www/jerney/
 
 # --- Install backend dependencies ---
 echo "📦 Installing backend dependencies..."
